8cd6441636c957008cacbb48fd55e25ae8020db0
phoenixmcknight <phoenixmcknight@gmail.com>
2019-10-03 12:10:45 -0400

    creates project

---
+++ Unit4Project1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit4Project1
+//
+//  Created by Phoenix McKnight on 9/27/19.
+//  Copyright © 2019 Phoenix McKnight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Unit4Project1Tests/Unit4Project1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Unit4Project1Tests.swift
+//  Unit4Project1Tests
+//
+//  Created by Phoenix McKnight on 9/27/19.
+//  Copyright © 2019 Phoenix McKnight. All rights reserved.
+//
+
+import XCTest
+@testable import Unit4Project1
+
+class Unit4Project1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
